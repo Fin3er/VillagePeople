5d63f151bcee0f96e78568bf6a1b27497ffb278e
Ekstram <moonwolf83@hotmail.com>
2015-04-14 15:51:51 +0300

    Lisätty sivujen pohjat

---
+++ Mokkivarausjarjestelma/Kayttajat_lisaa_muokkaa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mokkivarausjarjestelma
+{
+    public partial class Kayttajat_lisaa_muokkaa : Form
+    {
+        public Kayttajat_lisaa_muokkaa()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Mokkivarausjarjestelma/Lisapalvelut_lisaa_muokkaa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mokkivarausjarjestelma
+{
+    public partial class Lisapalvelut_lisaa_muokkaa : Form
+    {
+        public Lisapalvelut_lisaa_muokkaa()
+        {
+            InitializeComponent();
+        }
+    }
+}
