ba0dc6ddb23d372b5f27865da5aca945591d4b68
eenokki <eenokki@gmail.com>
2015-05-03 03:29:04 +0300

    Mokkien peruskoodia.

--- Mokkivarausjarjestelma/Mokki.cs
+++ Mokkivarausjarjestelma/Mokki.cs
@@ -4,9 +4,179 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
 namespace Mokkivarausjarjestelma
 {
-    public class Mokki
+    public partial class Mokki
     {
+        /* laitoin joihinkin muuttujiin etuliitteen, koska ajattelin, 
+        että muissa osissa tulee samannimisiä muuttujia ja aiheuttaa epäselvyyksiä */
+        private string mokkitunnus;
+	    private string m_osoite;
+	    private string m_postinumero;
+	    private string m_toimipaikka;
+	    private string m_maa;
+	    private string vuorokausihinta;
+	    private string huoneita;
+	    private string makuuhuoneita;
+	    private string pintaala;
+	    private string vuodepaikat;
+	    private string verkkoyhteys;
+	    private string m_lisatietoa;
+		
+	public Mokki()
+	{
+	}
+	
+	~Mokki()
+	{
+	}
+
+    //Lista löytyneitä mökkejä varten
+    public List<Mokki> mokkilista = new List<Mokki>();
+
+    //Muuttujat tietokantakyselyjä varten
+    protected MySqlConnection yhteys;
+    protected MySqlCommand kasky;
+    protected MySqlDataReader lukija;
+
+    public void HaeKaikkiMokitTietokannasta()
+    {
+        Tietokanta t = new Tietokanta();
+        yhteys = t.YhdistaTietokantaan();
+        kasky = yhteys.CreateCommand();
+        kasky.CommandText = "Select * from mokki";
+        lukija = kasky.ExecuteReader();
+        try
+        {
+            while (lukija.Read())
+            {
+                //Luodaan, jokaista taulun riviä varten asiakasolioita 
+                Mokki m = new Mokki();
+                m.Mokkitunnus = lukija.GetString("mokkitunnus");
+                m.M_osoite = lukija.GetString("katuosoite");
+                m.M_postinumero = lukija.GetString("postinumero");
+                m.M_toimipaikka = lukija.GetString("toimipaikka");
+                m.M_maa = lukija.GetString("maa");
+                m.Vuorokausihinta = lukija.GetString("vuorokausihinta");
+                m.Huoneita = lukija.GetString("huoneita");
+                m.Makuuhuoneita = lukija.GetString("makuuhuoneita");
+                m.Pintaala = lukija.GetString("pinta-ala");
+                m.Vuodepaikat = lukija.GetString("vuodepaikat");
+                m.M_lisatietoa = lukija.GetString("lisatietoa");
+                m.Verkkoyhteys = lukija.GetString("verkkoyhteys");
+                //Lisätään luotu olio listaan
+                mokkilista.Add(m);
+                //Alustetaan t nollaksi
+                m = null;
+            }
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show("Tietoja haettaessa tapahtui virhe:" + ex.ToString());
+        }
+        try
+        {
+            //Suljetaan reader
+            lukija.Close();
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show("Readeria sulkiessa tapahtui virhe:" + ex.ToString());
+        }
+        t.SuljeYhteysTietokantaan(yhteys);
+    }
+
+    public void PaivitaMokkiTietokantaan(Mokki m)
+    {
+        Tietokanta t = new Tietokanta();
+        yhteys = t.YhdistaTietokantaan();
+        kasky = yhteys.CreateCommand();
+        //Update Query @merkityt muuttujat? korvataan parametreillä
+        kasky.CommandText = @"UPDATE mokki SET katuosoite=@katuosoite, postinumero=@postinumero,
+            toimipaikka=@toimipaikka, maa=@maa, vuorokausihinta=@vuorokaushinta, huoneita=@huoneita, makuuhuoneita=@makuuhuoneita, 
+            pintaala=@pintaala, vuodepaikat=@vuodepaikat, lisatietoa=@lisatietoa, verkkoyhteys=@verkkoyhteys WHERE mokkitunnus=@mokkitunnus";
+        //Lisätään updatequeryyn parametrina annetun asiakkaan tiedot
+        kasky.Parameters.AddWithValue("@mokkitunnus", m.Mokkitunnus);
+        kasky.Parameters.AddWithValue("@katuosoite", m.M_osoite);
+        kasky.Parameters.AddWithValue("@postinumero", m.M_postinumero);
+        kasky.Parameters.AddWithValue("@toimipaikka", m.M_toimipaikka);
+        kasky.Parameters.AddWithValue("@maa", m.M_maa);
+        kasky.Parameters.AddWithValue("@vuorokausihinta", m.Vuorokausihinta);
+        kasky.Parameters.AddWithValue("@huoneita", m.Huoneita);
+        kasky.Parameters.AddWithValue("@makuuhuoneita", m.Makuuhuoneita);
+        kasky.Parameters.AddWithValue("@pintaala", m.Pintaala);
+        kasky.Parameters.AddWithValue("@vuodepaikat", m.Vuodepaikat);
+        kasky.Parameters.AddWithValue("@lisatietoa", m.M_lisatietoa);
+        kasky.Parameters.AddWithValue("@verkkoyhteys", m.Verkkoyhteys);
+        try
+        {
+            kasky.ExecuteNonQuery();
+            //Viesti, joka ilmoittaa tietojen päivityksen onnistuneen
+            MessageBox.Show("Mokkitiedot päivitetty", "Vahvistus", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show("Päivitettäessä tietoja tapahtui virhe: " + ex.ToString());
+        }
+
+        //Suljetaan yhteys
+        t.SuljeYhteysTietokantaan(yhteys);
+    }
+
+    //Metodi asiakkaan tietokantaan lisäämiselle
+    public void LisaaMokkiTietokantaan(Mokki m)
+    {
+        Tietokanta t = new Tietokanta();
+        yhteys = t.YhdistaTietokantaan();
+        kasky = yhteys.CreateCommand();
+        kasky.CommandText = @"INSERT INTO mokki (katuosoite, postinumero, toimipaikka, maa, vuorokausihinta, huoneita, makuuhuoneita, pintaala, vuodepaikat, lisatietoa, verkkoyhteys)
+                                VALUES (@katuosoite, @postinumero, @toimipaikka, @maa, @vuorokaushinta, @huoneita, @makuuhuoneita, @pintaala, @vuodepaikat, @lisatietoa, @verkkoyhteys)";
+        kasky.Parameters.AddWithValue("@katuosoite", m.M_osoite);
+        kasky.Parameters.AddWithValue("@postinumero", m.M_postinumero);
+        kasky.Parameters.AddWithValue("@toimipaikka", m.M_toimipaikka);
+        kasky.Parameters.AddWithValue("@maa", m.M_maa);
+        kasky.Parameters.AddWithValue("@vuorokausihinta", m.Vuorokausihinta);
+        kasky.Parameters.AddWithValue("@huoneita", m.Huoneita);
+        kasky.Parameters.AddWithValue("@makuuhuoneita", m.Makuuhuoneita);
+        kasky.Parameters.AddWithValue("@pintaala", m.Pintaala);
+        kasky.Parameters.AddWithValue("@vuodepaikat", m.Vuodepaikat);
+        kasky.Parameters.AddWithValue("@lisatietoa", m.M_lisatietoa);
+        kasky.Parameters.AddWithValue("@verkkoyhteys", m.Verkkoyhteys);
+        try
+        {
+            kasky.ExecuteNonQuery();
+            //Viesti, joka ilmoittaa tietojen päivityksen onnistuneen
+            MessageBox.Show("Uusi mokki lisätty", "Vahvistus", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show("Tapahtui virhe lisättäessä mokkia:" + ex.ToString());
+        }
+        //Suljetaan yhteys
+        t.SuljeYhteysTietokantaan(yhteys);
+    }
+        //Metodi asiakkaan poistamiselle tietokannasta
+    public void PoistaMokkiTietokannasta(Mokki m)
+    {
+        Tietokanta t = new Tietokanta();
+        yhteys = t.YhdistaTietokantaan();
+        kasky = yhteys.CreateCommand();
+        kasky.CommandText = "DELETE FROM mokki WHERE mokkitunnus=@mokkitunnus";
+        kasky.Parameters.AddWithValue("@mokkitunnus", m.Mokkitunnus);
+        try
+        {
+            kasky.ExecuteNonQuery();
+            MessageBox.Show("Mokki poistettu", "Vahvistus", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show("Tapahtui virhe mokkia poistettaessa:" + ex.ToString());
+        }
+        t.SuljeYhteysTietokantaan(yhteys);
+    }
+
     }
 }
---
+++ Mokkivarausjarjestelma/MokkiGetSet.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mokkivarausjarjestelma
+{
+    public partial class Mokki
+	{
+	
+	public string Mokkitunnus
+	{
+		get {return mokkitunnus; }
+		set {mokkitunnus = value; }
+	}
+	
+	
+	public string M_osoite
+	{
+		get {return m_osoite; }
+		set {m_osoite = value; }
+	}
+	
+	
+	public string M_postinumero
+	{
+		get {return m_postinumero; }
+		set {m_postinumero = value; }
+	}
+
+	
+	public string M_toimipaikka
+	{
+		get {return m_toimipaikka; }
+		set {m_toimipaikka = value; }
+	}
+	
+	
+	public string M_maa
+	{
+		get {return m_maa; }
+		set {m_maa = value; }
+	}
+	
+	
+	public string Vuorokausihinta
+	{
+		get {return vuorokausihinta; }
+		set {vuorokausihinta = value; }
+	}
+	
+	
+	public string Huoneita
+	{
+		get {return huoneita; }
+		set {huoneita = value; }
+	}
+	
+	
+	public string Makuuhuoneita
+	{
+		get {return makuuhuoneita; }
+		set {makuuhuoneita = value; }
+	}
+	
+	
+	public string Pintaala
+	{
+		get {return pintaala; }
+		set {pintaala = value; }
+	}
+	
+
+	public string Vuodepaikat
+	{
+		get {return vuodepaikat; }
+		set {vuodepaikat = value; }
+	}
+	
+	
+	public string M_lisatietoa
+	{
+		get {return m_lisatietoa; }
+		set {m_lisatietoa = value; }
+	}
+        
+
+	public string Verkkoyhteys
+	{
+		get {return verkkoyhteys; }
+		set {verkkoyhteys = value; }
+	}
+	
+	}
+}
