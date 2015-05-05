e2510c66fc724c610eccedac1fae41051424eaad
Ekstram <moonwolf83@hotmail.com>
2015-05-05 10:15:21 +0300

    Perustiedot lisätty

--- Mokkivarausjarjestelma/Varaus.cs
+++ Mokkivarausjarjestelma/Varaus.cs
@@ -7,10 +7,13 @@ using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
 using System.Windows.Forms;
 
+/* Ei testattu, tehty Asiakas mallista */
+
 namespace Mokkivarausjarjestelma
 {
     public partial class Varaus
     {
+        // Varauksen muuttujat
         private string varausid;
         private Toimipiste toimipiste; 
         private Mokki mokki; 
@@ -24,5 +27,31 @@ namespace Mokkivarausjarjestelma
         private string yhteenveto;
         private string laskutus;
         private double hinta;
+
+        //Lista löytyneitä varauksia varten
+        public List<Varaus> varauslista = new List<Varaus>();
+
+        //Muuttujat tietokantakyselyjä varten
+        protected MySqlConnection yhteys;
+        protected MySqlCommand kasky;
+        protected MySqlDataReader lukija;
+
+        //Varaus konstruktori
+        public Varaus() { }
+        //Varaus destruktori
+        ~Varaus() { }
+
+        // Varauksen hakeminen
+
+
+        // Varauksen päivittäminen
+
+
+        // Varauksen lisääminen
+
+
+        // Varauksen poistaminen
+
+
     }
 }
