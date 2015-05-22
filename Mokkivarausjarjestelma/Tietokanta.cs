e32b8edbda8d24cf37e09e2266fbd791c661aa4e
Ekstram <moonwolf83@hotmail.com>
2015-05-22 14:59:13 +0300

    tietokantavaihtoa selvennetty

--- Mokkivarausjarjestelma/Tietokanta.cs
+++ Mokkivarausjarjestelma/Tietokanta.cs
@@ -57,8 +57,10 @@ namespace Mokkivarausjarjestelma
             //Yritetään avata yhteys
             try
             {
-                yhteys.ConnectionString = "server = 192.168.1.222; user id = newuser; password = uusi1234; database = tietokanta";
-                /*yhteys.ConnectionString = "server = 192.168.2.101; user id = newuser; password = uusi1234; database = villagepeople";*/
+                /* Mikko S */
+                /* yhteys.ConnectionString = "server = 192.168.1.222; user id = newuser; password = uusi1234; database = tietokanta";*/
+                /* Tuomas */ 
+                yhteys.ConnectionString = "server = 192.168.2.101; user id = newuser; password = uusi1234; database = villagepeople";
                 yhteys.Open();
                 MessageBox.Show("Yhdistetty MySql-tietokantaan");
                 return yhteys;
